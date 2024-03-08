096dabf7119b3011ec7c6703b645661823b25cfe
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-08 19:14:52 +0530

    Added boilerplate for our small handson react app...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Cards from "./Components/Cards";
+import Form from "./Components/Form";
+
+function App() {
+  return (
+    <div>
+      <Cards />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
